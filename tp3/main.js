8be2b651d1cbd264d4ce1d7506886ab0a71acca9
Quentin MAYER <quentin.mayer@ynov.com>
2019-01-07 16:49:05 +0100

    tp3

---
+++ tp3/main.js
@@ -0,0 +1,20 @@
+var ctx = document.getElementById('myChart').getContext('2d');
+var myBarChart = new Chart(ctx, {
+    type: 'bar',
+
+
+
+    // The data for our dataset
+    data: {
+        labels: ["January", "February", "March", "April", "May", "June", "July"],
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+    },
+
+    // Configuration options go here
+    options: {}
+});
